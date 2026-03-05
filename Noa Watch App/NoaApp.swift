9bc32c3d066a32a9f5ba92adf757f2b3f50bbdb2
Oein <oein@oein.kr>
2026-03-05 20:40:36 +0900

    Initial Commit

---
+++ Noa Watch App/NoaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoaApp.swift
+//  Noa Watch App
+//
+//  Created by oein on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Noa_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Noa/NoaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoaApp.swift
+//  Noa
+//
+//  Created by oein on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct NoaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
